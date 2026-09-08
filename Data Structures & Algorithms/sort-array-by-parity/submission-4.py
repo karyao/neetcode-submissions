c46092c08286094f64469bbda9a8b42a7857b056
Karen Yao <75444418+karyao@users.noreply.github.com>
2026-09-08 16:41:50 -0700

    Add: sort-array-by-parity - submission-4

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        # even numbers | odd numbers 
+        left = 0
+        right = len(nums) - 1
+        while left < right: 
+            if nums[left] % 2 == 1 and nums[right] % 2 == 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+            if nums[left] % 2 == 0: 
+                left += 1
+
+            if nums[right] % 2 == 1:
+                right -= 1
+            
+
+        return nums 
